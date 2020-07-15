e41d427e9c84c9db060b22afd22d8610c5d691bd
Ashutosh Pandey <pandeyashutosh150799@gmail.com>
2020-07-15 15:50:37 +0530

    Updates

---
+++ precache-manifest.04ac5ac8398087ef44235c3eda902273.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a6183f9792f5fc3897baf2190f83f1",
+    "url": "/ChatBrat/index.html"
+  },
+  {
+    "revision": "ee1cbbd97c33d67cf9ff",
+    "url": "/ChatBrat/static/css/main.c3e481d3.chunk.css"
+  },
+  {
+    "revision": "2996487d2745fcba9361",
+    "url": "/ChatBrat/static/js/2.9f7b7652.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/ChatBrat/static/js/2.9f7b7652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1cbbd97c33d67cf9ff",
+    "url": "/ChatBrat/static/js/main.1a4f01de.chunk.js"
+  },
+  {
+    "revision": "5a4e9b06f98635fca5f4",
+    "url": "/ChatBrat/static/js/runtime-main.a54a94d8.js"
+  }
+]);
